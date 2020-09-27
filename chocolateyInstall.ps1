8a3c3849dd1bd34baa1b05e1448bb1162b90b731
Daniel Ziltener <dziltener@lyrion.ch>
2020-09-27 19:17:30 +0200

    Installation things

--- chocolateyInstall.ps1
+++ chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-C:\tools\msys64\usr\bin\env MSYSTEM=MINGW64 /bin/bash -l -c 'pacman -S --needed --noconfirm pactoys'
-C:\tools\msys64\usr\bin\env MSYSTEM=MINGW64 /bin/bash -l -c 'pacboy -Sy --needed --noconfirm binutils:x make:x gcc:x gettext:x readline:x patch'
+C:\tools\msys64\usr\bin\env MSYSTEM=MINGW64 /bin/bash -l -c 'pacman -S --needed --noconfirm pactoys patch'
+C:\tools\msys64\usr\bin\env MSYSTEM=MINGW64 /bin/bash -l -c 'pacboy -Sy --needed --noconfirm binutils:x make:x gcc:x gettext:x readline:x'
 New-Item $(Get-ToolsLocation) -ItemType Directory
 Move-Item $env:chocolateyPackageFolder\tools $(Get-ToolsLocation)\chicken -Force
 Install-ChocolateyEnvironmentVariable -VariableName "CHICKEN_PREFIX" -VariableValue "$(Get-ToolsLocation)\chicken" -VariableType Machine
