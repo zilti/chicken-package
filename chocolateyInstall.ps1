6a2e84a7fa63341f8dd7d157bab59a65b8b110ac
Daniel Ziltener <dziltener@lyrion.ch>
2025-02-10 01:23:08 +0100

    Remove Chocolatey Private Env Var

--- chocolateyInstall.ps1
+++ chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 C:\tools\msys64\usr\bin\env MSYSTEM=MINGW64 /bin/bash -l -c 'pacman -S --needed --noconfirm pactoys patch'
 C:\tools\msys64\usr\bin\env MSYSTEM=MINGW64 /bin/bash -l -c 'pacboy -Sy --needed --noconfirm binutils:x make:x gcc:x gettext:x readline:x'
 New-Item $(Get-ToolsLocation) -ItemType Directory
-Move-Item $env:chocolateyPackageFolder\tools $(Get-ToolsLocation)\chicken -Force
+Move-Item $(Get-ChocolateyPath -PathType 'PackagePath')\tools $(Get-ToolsLocation)\chicken -Force
 Install-ChocolateyEnvironmentVariable -VariableName "CHICKEN_PREFIX" -VariableValue "$(Get-ToolsLocation)\chicken" -VariableType Machine
 Install-ChocolateyPath -PathToInstall "$(Get-ToolsLocation)\chicken\bin" -PathType Machine
 Install-ChocolateyPath -PathToInstall "$(Get-ToolsLocation)\msys64\mingw64\bin" -PathType Machine
